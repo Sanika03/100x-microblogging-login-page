6a7fe0e4f608a0d1904fd0e1d1a1a4d7fac359c6
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-10-31 01:26:47 +0530

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue": "#1D9BF0",
+        "neutral-50": "#F9F9F9",
+        "neutral-700": "#404040",
+        "neutral-1000": "#000000",
+        "stroke-50": "rgba(29, 155, 240, 0.24)"
+    },
   },
   plugins: [],
+  }
 }
-
